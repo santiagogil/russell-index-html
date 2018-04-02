ebd08e7831ec7f42b2c04ef768ecf83d6de06134
Santiago Gil <gil.educaciontdf@gmail.com>
2018-04-02 11:52:22 -0300

    fix(cli): Fix icons path
    Move it to our usual asset folder

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 module.exports = function template () {
   return `<html lang='es'><head>
 <meta charset="UTF-8">
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-<link rel="manifest" href="/site.webmanifest">
-<link rel="mask-icon" href="/safari-pinned-tab.svg" color="{{themecolor}}">
-<link rel="shortcut icon" href="/favicon.ico">
-<meta name="msapplication-TileColor" content="#9f00a7">
+<link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon-16x16.png">
+<link rel="manifest" href="/assets/site.webmanifest">
+<link rel="mask-icon" href="/assets/safari-pinned-tab.svg" color="{{themecolor}}">
+<link rel="shortcut icon" href="/assets/favicon.ico">
+<meta name="msapplication-TileColor" content="{{themecolor}}">
 <!-- Chrome, Firefox OS and Opera -->
 <meta name="theme-color" content="{{themecolor}}">
 <!-- Windows Phone -->
